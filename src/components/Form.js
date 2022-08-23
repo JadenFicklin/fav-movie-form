b18a7a832cdd8709e6023d5038a0a0fec4a1a3c2
JadenFicklin <jadendficklin@gmail.com>
2022-08-23 13:39:36 -0600

    updates to nav bar, tied in state to the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+  const [movieTitle, setMovieTitle] = useState("");
+  const [movieRating, setMovieRating] = useState(0);
+  const [reccomend, setReccomend] = useState(true);
+  const [isDrama, setIsDrama] = useState(false);
+  const [isComedy, setIsComedy] = useState(false);
+  const [isAction, setIsAction] = useState(false);
+  const [isFantasy, setIsFantasy] = useState(false);
+  const [isHorror, setIsHorror] = useState(false);
+  const [isRomance, setIsRomance] = useState(false);
+  const [isWestern, setIsWestern] = useState(false);
+  const [isThriller, setIsThriller] = useState(false);
+
+  const submitMovie = (e) => {
+    e.preventDefault();
+
+    const form = {
+      movieTitle,
+      movieRating,
+      reccomend,
+      isDrama,
+      isComedy,
+      isAction,
+      isFantasy,
+      isHorror,
+      isRomance,
+      isWestern,
+      isThriller,
+    };
+
+
+  };
+
   return (
     <>
       <div className="form-outer">
@@ -8,49 +40,128 @@ function Form() {
           <h1 className="form-top">Movie Form</h1>
           <form className="form-middle">
             <h3>Whats a movie you like?</h3>
-            <input type="text" />
+            <input
+              type="text"
+              value={movieTitle}
+              onChange={({ target }) => setMovieTitle(target.value)}
+            />
             <h3>How would you rate this movie?</h3>
             <div>
-              <input type="range" id="volume" name="volume" min="0" max="10" />
+              <input
+                type="range"
+                id="volume"
+                name="volume"
+                min="0"
+                max="10"
+                onChange={({ target }) => setMovieRating(target.value)}
+                value={movieRating}
+              />
+              {movieRating}
             </div>
             <h3>Would you reccomend this movie to a friend?</h3>
-            <input type="radio" id="yes" name="would_recommend" value="yes" />
+            <input
+              type="radio"
+              id="yes"
+              name="would_recommend"
+              value="yes"
+              checked={reccomend}
+              onClick={() => setReccomend(true)}
+            />
             <label for="html">Yes</label>
-            <input type="radio" id="no" name="would_recommend" value="no" />
+            <input
+              type="radio"
+              id="no"
+              name="would_recommend"
+              value="no"
+              checked={!reccomend}
+              onClick={() => setReccomend(false)}
+            />
             <label for="css">No</label>
             <h3>What genres fit for this movie?</h3>
-            <input type="checkbox" id="genre1" name="genre1" value="drama" />
+            <input
+              type="checkbox"
+              id="genre1"
+              name="genre1"
+              value={isDrama}
+              onChange={() => setIsDrama(!isDrama)}
+            />
             <label for="genre1">Drama</label>
             <br></br>
-            <input type="checkbox" id="genre2" name="genre2" value="comedy" />
+            <input
+              type="checkbox"
+              id="genre2"
+              name="genre2"
+              value={isComedy}
+              onChange={() => setIsComedy(!isComedy)}
+            />
             <label for="genre2">Comedy</label>
             <br></br>
 
-            <input type="checkbox" id="genre3" name="genre3" value="action" />
+            <input
+              type="checkbox"
+              id="genre3"
+              name="genre3"
+              value={isAction}
+              onChange={() => setIsAction(!isAction)}
+            />
             <label for="genre3">Action</label>
             <br></br>
 
-            <input type="checkbox" id="genre4" name="genre4" value="fantasy" />
+            <input
+              type="checkbox"
+              id="genre4"
+              name="genre4"
+              value={isFantasy}
+              onChange={() => setIsFantasy(!isFantasy)}
+            />
             <label for="genre4">Fantasy</label>
             <br></br>
 
-            <input type="checkbox" id="genre5" name="genre5" value="horror" />
+            <input
+              type="checkbox"
+              id="genre5"
+              name="genre5"
+              value={isHorror}
+              onChange={() => setIsHorror(!isHorror)}
+            />
             <label for="genre5">Horror</label>
             <br></br>
 
-            <input type="checkbox" id="genre6" name="genre6" value="romance" />
+            <input
+              type="checkbox"
+              id="genre6"
+              name="genre6"
+              value={isRomance}
+              onChange={() => setIsRomance(!isRomance)}
+            />
             <label for="genre6">Romance</label>
             <br></br>
 
-            <input type="checkbox" id="genre7" name="genre7" value="western" />
+            <input
+              type="checkbox"
+              id="genre7"
+              name="genre7"
+              value={isWestern}
+              onChange={() => setIsWestern(!isWestern)}
+            />
             <label for="genre7">Western</label>
             <br></br>
 
-            <input type="checkbox" id="genre8" name="genre8" value="thriller" />
+            <input
+              type="checkbox"
+              id="genre8"
+              name="genre8"
+              value={isThriller}
+              onChange={() => setIsThriller(!isThriller)}
+            />
             <label for="genre8">Thriller</label>
             <br></br>
             <br></br>
-            <input type="submit" className="submit" />
+            <input
+              type="submit"
+              className="submit"
+              onClick={(e) => submitMovie(e)}
+            />
           </form>
         </div>
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,14 +6,29 @@ function Nav() {
   const [form, setForm] = useState(true);
   console.log(form);
 
+  const formTab = {
+    backgroundColor: form ? "rgb(77, 77, 77)" : "rgb(104, 104, 104)",
+  };
+  const formTabTwo = {
+    backgroundColor: !form ? "rgb(77, 77, 77)" : "rgb(104, 104, 104)",
+  };
+
   return (
     <>
       <div className="nav-outer">
         <div className="nav-top">
-          <div className="nav-create-form" onClick={() => setForm(true)}>
+          <div
+            className="nav-create-form"
+            onClick={() => setForm(true)}
+            style={formTab}
+          >
             Create form
           </div>
-          <div className="nav-view-forms" onClick={() => setForm(false)}>
+          <div
+            className="nav-view-forms"
+            onClick={() => setForm(false)}
+            style={formTabTwo}
+          >
             View forms
           </div>
         </div>
